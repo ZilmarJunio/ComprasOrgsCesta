71afb39f08f4abb7aa9724a403a3d33b227ff8c0
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-06-09 11:28:59 -0300

    desenvolvendo descriçao

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, SafeAreaView } from 'react-native';
+import React from 'react';
+import Cesta from './src/telas/cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styleText.container}>00000
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar/>
+      <Cesta/>
+    </SafeAreaView>
   );
 }
-
-const styleText = StyleSheet.create({
-
-  container: {
-    color:'#fff',
-  }
-
-})
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React from 'react';
+import {StyleSheet, Image, Dimensions, Text, View} from 'react-native';
 
-function Cesta() {
-    return <Text>Cesta</Text>
+import top from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Cesta() {
+    return <>
+    <Image source={top} style={styles.top} />
+    <Text style={styles.title}>Detalhes da Compra</Text>
+
+    <View>
+        <Text>Cesta de Verduras</Text>
+        <Text>Fazenda dú Zilmá</Text>
+        <Text>Os melhores produtos selecionados cuidadosamente da fazenda para a sua cozinha, uai!</Text>
+    </View>
+
+    </>
 }
+
+const styles = StyleSheet.create({
+
+    top: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+
+    title: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+
+});
